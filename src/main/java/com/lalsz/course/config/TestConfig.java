3cf59728880a6f9179705714bcc78d2e62028ad8
leonardo alves <109185275+Leonardolasz@users.noreply.github.com>
2024-01-30 14:17:23 -0300

    OrderStatus enum

--- src/main/java/com/lalsz/course/config/TestConfig.java
+++ src/main/java/com/lalsz/course/config/TestConfig.java
@@ -2,6 +2,7 @@ package com.lalsz.course.config;
 
 import com.lalsz.course.entities.User;
 import com.lalsz.course.entities.Order;
+import com.lalsz.course.entities.enums.OrderStatus;
 import com.lalsz.course.repositories.OrderRepository;
 import com.lalsz.course.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -29,9 +30,9 @@ public class TestConfig implements CommandLineRunner {
         User u1 = new User(null, "Drew Lock", "drew@gmail.com", "988888888", "123456");
         User u2 = new User(null, "J.J. McCarthy", "mccarthy@gmail.com", "977777777", "123456");
 
-        Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), u1);
-        Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), u2);
-        Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), u1);
+        Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.PAID, u1);
+        Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.WAITING_PAIMENT, u2);
+        Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.WAITING_PAIMENT, u1);
 
         userRepository.saveAll(Arrays.asList(u1, u2));
         orderRepository.saveAll(Arrays.asList(o1, o2, o3));
