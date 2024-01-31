7dccff0758d7b719b5a76dc11bee74fe11579f09
leonardo alves <109185275+Leonardolasz@users.noreply.github.com>
2024-01-31 15:24:33 -0300

    User delete

--- src/main/java/com/lalsz/course/resources/UserResource.java
+++ src/main/java/com/lalsz/course/resources/UserResource.java
@@ -29,10 +29,16 @@ public class UserResource {
         return ResponseEntity.ok().body(obj);
     }
 
-    @PostMapping(value = "")
+    @PostMapping
     public ResponseEntity<User> insert(@RequestBody User obj) {
         obj = service.insert(obj);
         URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
         return ResponseEntity.created(uri).body(obj);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        service.delete(id);
+        return ResponseEntity.noContent().build();
     }
 }
